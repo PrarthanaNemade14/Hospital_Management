62fb2ce2209dbbcb7ea6dcf3e733d54cc8a24b8c
PrarthanaNemade14 <115116403+PrarthanaNemade14@users.noreply.github.com>
2022-10-28 23:22:52 -0400

    Add files via upload

---
+++ AED_Assignment2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.aed_assignment2;
+
+/**
+ *
+ * @author nemad
+ */
+public class AED_Assignment2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
